34a18715c3c1cc44245a64c3986a9a316923c259
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-06-18 11:50:00 -0700

    :heavy_check_mark: Make pass all tests
    Change minutes and seconds state for timer and make the proper conversions

--- src/App.js
+++ src/App.js
@@ -15,11 +15,12 @@ function App() {
   // Hooks
   const [breakLen, setBreakLen] = useState(BREAK_LENGTH)
   const [sessionLen, setSessionLen] = useState(SESSION_LENGTH)
-  const [minutes, setMinutes] = useState(sessionLen)
-  const [seconds, setSecond] = useState(0)
+  // const [minutes, setMinutes] = useState(sessionLen)
+  // const [seconds, setSecond] = useState(0)
   const [timerType, setTimerType] = useState(SESSION_TEXT)
   const [isTimerRunning, setIsTimerRunning] = useState(false)
-  // local functions
+  const [timer, setTimer] = useState(sessionLen * 60)
+
   const incrementBreakLen = () => {
     if (breakLen < 60) {
       setBreakLen(breakLen + 1)
@@ -32,53 +33,55 @@ function App() {
   }
   const incrementSessionLen = () => {
     if (sessionLen < 60) {
-      setMinutes(sessionLen + 1)
+      // setMinutes(sessionLen + 1)
       setSessionLen(sessionLen + 1)
+      if (!isTimerRunning) setTimer(sessionLen * 60 + 60)
     }
   }
   const decrementSessionLen = () => {
     if (sessionLen > 1) {
-      setMinutes(sessionLen - 1)
+      // setMinutes(sessionLen - 1)
       setSessionLen(sessionLen - 1)
+      if (!isTimerRunning) setTimer(sessionLen * 60 - 60)
     }
   }
 
   const reset = () => {
+    setIsTimerRunning(false)
     setBreakLen(BREAK_LENGTH)
     setSessionLen(SESSION_LENGTH)
-    setSecond(0)
-    setIsTimerRunning(false)
     setTimerType(SESSION_TEXT)
-    setMinutes(SESSION_LENGTH)
     stopSound('beep')
+    setTimer(SESSION_LENGTH * 60)
   }
 
   const startStop = () => {
-    if (isTimerRunning) stopSound('beep')
+    if (isTimerRunning) {
+      stopSound('beep')
+      // setTimer(sessionLen * 60)
+    }
     setIsTimerRunning(!isTimerRunning)
   }
   const tick = () => {
-    if (seconds === 0 && minutes === 0) {
-      if (timerType === BREAK_TEXT) {
-        setTimerType(SESSION_TEXT)
-        setMinutes(sessionLen)
-      } else {
+    // switch timer
+    setTimer(timer - 1)
+    if (timer === 0) {
+      if (timerType === SESSION_TEXT) {
+        setTimer(breakLen * 60)
         setTimerType(BREAK_TEXT)
-        setMinutes(breakLen)
+      } else {
+        setTimer(sessionLen * 60)
+        setTimerType(SESSION_TEXT)
       }
-    }
-    // last 59 seconds
-    if (minutes === 0) {
       playSound('beep')
     }
-    if (seconds === 0) {
-      if (minutes > 0) {
-        setMinutes(minutes - 1)
-      }
-      setSecond(59)
-    } else {
-      setSecond(seconds - 1)
-    }
+  }
+  const parseTime = () => {
+    let minutes = Math.floor(timer / 60)
+    let seconds = timer - minutes * 60
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    return minutes + ':' + seconds
   }
   useInterval(tick, isTimerRunning ? 1000 : null)
 
@@ -95,7 +98,7 @@ function App() {
         increment={incrementSessionLen}
         decrement={decrementSessionLen}
       />
-      <Timer minutes={minutes} seconds={seconds} text={timerType} />
+      <Timer time={parseTime()} text={timerType} />
       <TimerControls reset={reset} startStop={startStop} />
       <audio id="beep" src={beep} />
     </div>
